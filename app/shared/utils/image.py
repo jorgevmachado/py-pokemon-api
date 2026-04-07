74505bfef1c8f7fd75e5b8b757a4318ebbcfefa4
jorge.machado-Kraken <jorge.vmachado@hotmail.com>
2026-04-07 11:19:22 -0300

    pad order numbers in ensure_external_image function and add corresponding tests

--- app/shared/utils/image.py
+++ app/shared/utils/image.py
@@ -1,4 +1,5 @@
 def ensure_external_image(order: int | None) -> str:
     if not order:
         return ''
-    return f'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/{order}.png'
+    formatted_order = str(order).zfill(3)
+    return f'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/{formatted_order}.png'
--- tests/app/shared/utils/test_image.py
+++ tests/app/shared/utils/test_image.py
@@ -3,9 +3,29 @@ from app.shared.utils.image import ensure_external_image
 
 class TestImageEnsureExternalImage:
     @staticmethod
-    def test_ensure_external_image():
-        image = 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/1.png'
+    def test_ensure_external_image_should_pad_single_digit_order():
+        image = 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/001.png'
         assert ensure_external_image(1) == image
+
+    @staticmethod
+    def test_ensure_external_image_should_pad_three_as_four_digits():
+        image = 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/003.png'
+        assert ensure_external_image(3) == image
+
+    @staticmethod
+    def test_ensure_external_image_should_pad_two_digit_order():
+        image = 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/085.png'
+        assert ensure_external_image(85) == image
+
+    @staticmethod
+    def test_ensure_external_image_should_pad_three_digit_order():
+        image = 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/101.png'
+        assert ensure_external_image(101) == image
+
+    @staticmethod
+    def test_ensure_external_image_should_keep_four_digit_order():
+        image = 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/1011.png'
+        assert ensure_external_image(1011) == image
 
     @staticmethod
     def test_ensure_external_image_not_has_order():
